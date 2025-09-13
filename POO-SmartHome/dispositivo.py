fedb843dd06947e6a61875e75dac3d2b7e464b0a
DiegoMendia <diegomendia21@gmail.com>
2025-09-12 22:20:20 -0300

    se agregue clase aireAcondicionado

--- POO-SmartHome/dispositivo.py
+++ POO-SmartHome/dispositivo.py
@@ -1,20 +1,43 @@
 import uuid
+from abc import ABC, abstractmethod
 
 
-class Dispositivo:
-    def __init__(self, id_vivienda: uuid.UUID, id_rutina: uuid.UUID = None, rutina=None):
-        self.id = uuid.uuid4()
-        self.id_vivienda = id_vivienda
-        self.id_rutina = id_rutina
-        self.estado = False
-        self.rutina = rutina
+class Dispositivo(ABC):
+    def _init_(self, id_vivienda: uuid.UUID, id_rutina: uuid.UUID = None, rutina=None):
+        self._id = uuid.uuid4()
+        self._id_vivienda = id_vivienda
+        self._id_rutina = id_rutina
+        self._estado = False
+        self._rutina = rutina
 
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def id_vivienda(self):
+        return self._id_vivienda
+
+    @property
+    def id_rutina(self):
+        return self._id_rutina
+
+    @property
+    def estado(self):
+        return self._estado
+
+    @property
+    def rutina(self):
+        return self._rutina
+
+    @abstractmethod
     def encender(self):
-        self.estado = True
+        pass
 
+    @abstractmethod
     def apagar(self):
-        self.estado = False
+        pass
 
     def asignar_rutina(self, rutina):
-        self.rutina = rutina
-        self.id_rutina = getattr(rutina, "id", uuid.uuid4())
+        self._rutina = rutina
+        self._id_rutina = getattr(rutina, "id", uuid.uuid4())
